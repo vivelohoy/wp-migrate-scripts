357175c626e9a32f95e7c0ce909f67fc8bbb43dc
Nick Bennett <nbennett@vivelohoy.com>
2014-07-10 10:48:40 -0500

    Add SQL scripts to drop extraneous plugin and sub-blog tables.

---
+++ drop_plugin_tables.sql
@@ -0,0 +1,16 @@
+# These tables are extraneous, created by plugins that we won't carry forward
+#
+# Plugin: Popularity Contest (https://wordpress.org/plugins/popularity-contest/)
+DROP TABLE IF EXISTS wp_ak_popularity;
+DROP TABLE IF EXISTS wp_ak_popularity_options;
+# Plugin: Gravity Forms (http://www.gravityforms.com/)
+DROP TABLE IF EXISTS wp_rg_form;
+DROP TABLE IF EXISTS wp_rg_form_meta;
+DROP TABLE IF EXISTS wp_rg_form_view;
+DROP TABLE IF EXISTS wp_rg_lead;
+DROP TABLE IF EXISTS wp_rg_lead_detail;
+DROP TABLE IF EXISTS wp_rg_lead_detail_long;
+DROP TABLE IF EXISTS wp_rg_lead_notes;
+# Plugin: YARPP (http://www.yarpp.com/)
+DROP TABLE IF EXISTS wp_yarpp_keyword_cache;
+DROP TABLE IF EXISTS wp_yarpp_related_cache;
---
+++ drop_sub_blog_tables.sql
@@ -0,0 +1,45 @@
+# These tables all belong to sub-blogs of vivelohoy.com, which are defunct
+# and we will not be migrating to the next platform.
+#
+# This table list was generated with the SQL query:
+# SELECT TABLE_NAME 
+# FROM information_schema.tables 
+# WHERE 
+# 	TABLE_SCHEMA LIKE "tmp_vivelohoy_1" AND
+# 	TABLE_NAME REGEXP 'wp\_[2345]\_.*'; 
+DROP TABLE IF EXISTS wp_2_commentmeta;
+DROP TABLE IF EXISTS wp_2_comments;
+DROP TABLE IF EXISTS wp_2_links;
+DROP TABLE IF EXISTS wp_2_options;
+DROP TABLE IF EXISTS wp_2_postmeta;
+DROP TABLE IF EXISTS wp_2_posts;
+DROP TABLE IF EXISTS wp_2_term_relationships;
+DROP TABLE IF EXISTS wp_2_term_taxonomy;
+DROP TABLE IF EXISTS wp_2_terms;
+DROP TABLE IF EXISTS wp_3_commentmeta;
+DROP TABLE IF EXISTS wp_3_comments;
+DROP TABLE IF EXISTS wp_3_links;
+DROP TABLE IF EXISTS wp_3_options;
+DROP TABLE IF EXISTS wp_3_postmeta;
+DROP TABLE IF EXISTS wp_3_posts;
+DROP TABLE IF EXISTS wp_3_term_relationships;
+DROP TABLE IF EXISTS wp_3_term_taxonomy;
+DROP TABLE IF EXISTS wp_3_terms;
+DROP TABLE IF EXISTS wp_4_commentmeta;
+DROP TABLE IF EXISTS wp_4_comments;
+DROP TABLE IF EXISTS wp_4_links;
+DROP TABLE IF EXISTS wp_4_options;
+DROP TABLE IF EXISTS wp_4_postmeta;
+DROP TABLE IF EXISTS wp_4_posts;
+DROP TABLE IF EXISTS wp_4_term_relationships;
+DROP TABLE IF EXISTS wp_4_term_taxonomy;
+DROP TABLE IF EXISTS wp_4_terms;
+DROP TABLE IF EXISTS wp_5_commentmeta;
+DROP TABLE IF EXISTS wp_5_comments;
+DROP TABLE IF EXISTS wp_5_links;
+DROP TABLE IF EXISTS wp_5_options;
+DROP TABLE IF EXISTS wp_5_postmeta;
+DROP TABLE IF EXISTS wp_5_posts;
+DROP TABLE IF EXISTS wp_5_term_relationships;
+DROP TABLE IF EXISTS wp_5_term_taxonomy;
+DROP TABLE IF EXISTS wp_5_terms;
