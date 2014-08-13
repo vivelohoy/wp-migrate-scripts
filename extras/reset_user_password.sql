e6d615c4d101acd21839e983ba64a717542ff127
Nick Bennett <nick@goggl.es>
2014-08-13 16:16:48 -0500

    Update reset_user_password.sql

--- extras/reset_user_password.sql
+++ extras/reset_user_password.sql
@@ -1,7 +1,7 @@
 -- Use the exact username as it appears in the user's WordPress profile
 UPDATE wp_users
-    SET user_pass = MD5(NEW PASSWORD HERE IN SINGLE QUOTES),
-    WHERE user_name = USERNAME IN SINGLE QUOTES;
+    SET user_pass = MD5(NEW PASSWORD HERE IN SINGLE QUOTES)
+    WHERE user_login = USERNAME IN SINGLE QUOTES;
 -- For example:
 -- UPDATE wp_users
 --     SET user_pass = MD5('deportEs!2014'),
