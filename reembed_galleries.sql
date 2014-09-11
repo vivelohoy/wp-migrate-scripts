18f373f673b980708130d3f5e02578e0f1c83d21
Nick Bennett <nbennett@vivelohoy.com>
2014-09-11 13:08:05 -0500

    Remove comments in reembed_galleries.sql that were problematic.

--- reembed_galleries.sql
+++ reembed_galleries.sql
@@ -1,20 +1,3 @@
--- wp_terms
------------
--- SELECT term_id FROM wp_terms WHERE slug = 'fotogalerias';
-
--- wp_term_taxonomy
--------------------
--- SELECT term_taxonomy_id FROM wp_term_taxonomy WHERE term_id = (SELECT term_id FROM wp_terms WHERE slug = 'fotogalerias');
-
--- wp_term_relationships
-------------------------
--- SELECT object_id FROM wp_term_relationships
---     WHERE term_taxonomy_id = (SELECT term_taxonomy_id FROM wp_term_taxonomy
---                                 WHERE term_id = (SELECT term_id FROM wp_terms
---                                                     WHERE slug = 'fotogalerias'));
-
--- wp_posts
------------
 UPDATE wp_posts
     JOIN (SELECT ID FROM wp_posts
             WHERE post_status = 'publish' AND
